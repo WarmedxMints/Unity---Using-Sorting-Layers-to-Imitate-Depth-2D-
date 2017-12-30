0eae6e136005d20dc85d14d02311dd037b8e09cf
Andrew(WarmedMints) <a.timms@dsl.pipex.com>
2017-12-30 19:05:31 +0000

    Initial Commit

---
+++ Assets/Scripts/DepthByYPos.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthByYPos : MonoBehaviour {
+
+    private const int DepthPerYUnit = 100;
+    [SerializeField]
+    private Renderer _renderer;
+
+    public void Update()
+    {
+        _renderer.sortingOrder = -(int)(transform.position.y * DepthPerYUnit);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 2f;
+
+	public void Update ()
+    {
+	    transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Speed * Time.deltaTime);
+    }
+}
